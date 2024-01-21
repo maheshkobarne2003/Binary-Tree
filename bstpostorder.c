f523ab45b0a05ae0606d733e9eebbcc54fef1dfe
Mahesh Kobarne <138373367+maheshkobarne2003@users.noreply.github.com>
2024-01-21 22:41:18 +0530

    display binary search tree by postorder recursive method

---
+++ bstpostorder.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node *left,*right;
+};
+
+struct node *create(struct node *root)
+{
+    int i,n,num;
+    struct node *parent,*newnode,*temp;
+    printf("enter limit:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        newnode=(struct node *)malloc(sizeof(struct node));
+        printf("enter value:");
+        scanf("%d",&num);
+        newnode->data=num;
+        newnode->left=NULL;
+        newnode->right=NULL;
+        if(root==NULL)
+        {
+            root=newnode;
+            continue;
+        } 
+        temp=root; 
+        while(temp!=NULL) 
+        {
+            parent=temp;  
+            if(num<temp->data)
+               temp=temp->left;
+            else
+               temp=temp->right;
+        }
+        if(num<parent->data)
+           parent->left=newnode;
+        else
+           parent->right=newnode;
+    }
+    return root;
+}
+
+void postorder(struct node *root)
+{
+    struct node *temp=root;
+    if(temp!=NULL)
+    {
+        postorder(temp->left); 
+        postorder(temp->right);
+        printf("%d\t",temp->data);
+    }
+}
+
+int main()
+{
+    struct node *root=NULL;
+    root=create(root);
+    printf("\ndisplay postorder:");
+    postorder(root);
+}
